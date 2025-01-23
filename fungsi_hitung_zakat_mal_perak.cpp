6762c861915eefcd523b1833f7bbe440ef3a5f4c
Naufal Aziz <nopalajez15@gmail.com>
2025-01-23 09:02:37 +0700

    Create fungsi_hitung_zakat_mal_perak.cpp

---
+++ fungsi_hitung_zakat_mal_perak.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// Fungsi untuk menghitung zakat mal perak
+double hitungZakatMalPerak(double totalHarta, double hargaPerak){
+    double nisab = 595 * hargaPerak;
+    if (totalHarta < nisab){
+        cout << "Anda belum diwajibkan membayar Zakat Mal Perak, karna belum mencapai Nishab" << endl;
+        return 0;
+    } else {
+        return totalHarta * 0.025;
+    }
+}
+
+int main (){
+  double totalHarta, hargaPerak;
+  cout << "Masukkan total harta anda (Rp) : ";
+  cin >> totalHarta;
+  cout << "Masukkan harga perak sekarang dalam per gram : ";
+  cin >> hargaPerak;
+  double zakatMal = hitungZakatMalPerak(totalHarta, hargaPerak);
+  cout << "Zakat mal yang harus anda keluarkan : Rp" << zakatMal << endl;
+}
